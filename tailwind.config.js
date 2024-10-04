a30f220be825ab5763c3b6aaf4829fd236e47210
Alix Peigue <irobot.moi@gmail.com>
2024-10-04 14:33:42 +0200

    Add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: 'var(--primary)',
+      primary: 'var(--primary)',
+      secondary: 'var(--secondary)',
+      background: 'var(--background)',
+    },
   },
   plugins: [],
 }
